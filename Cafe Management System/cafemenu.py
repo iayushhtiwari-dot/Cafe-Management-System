0c6381397a531ba4dcd32929c557c957dd74cb5c
iayushhtiwari-dot <iayushhtiwari@gmail.com>
2026-02-16 23:16:25 +0530

    Update cafemenu.py

--- Cafe Management System/cafemenu.py
+++ Cafe Management System/cafemenu.py
@@ -1,1 +1,14 @@
-
+menu = {
+    "Cappuccino": 159,
+    "Hot Café Latte": 169,
+    "Café Americano": 159,
+    "Café Mocha": 189,
+    "Filter Coffee": 159,
+    "Vanilla Cappuccino": 169,
+    "Espresso Shot": 149,
+    "King Cappuccino": 189,
+    "King Latte": 199,
+    "Hot Ethiopian Latte": 259,
+    "Honey Cinnamon Coffee": 159,
+    "Flat White Coffee": 169,
+    "Turmeric Ginger Cappuccino": 169}
