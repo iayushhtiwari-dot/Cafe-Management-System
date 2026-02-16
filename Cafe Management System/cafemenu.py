b9433da03b18be801617db072990c8aca059cd07
iayushhtiwari-dot <iayushhtiwari@gmail.com>
2026-02-16 23:19:10 +0530

    Enhance ordering process in cafemenu.py

--- Cafe Management System/cafemenu.py
+++ Cafe Management System/cafemenu.py
@@ -25,3 +25,19 @@ if order in menu:
         print(f"Added {order} to your order. Current total: ₹{order_total}")
 else:
         print("Sorry, that item is not on the menu.")
+
+more = input("Would you like to order anything else?: (yes/no)")
+while more.lower() == "yes":
+        another_order = input("Please enter the name of the coffee you would like to order: ")
+        if another_order in menu:
+            order_total += menu[another_order]
+            print(f"Added {another_order} to your order. Current total: ₹{order_total}")
+        else:
+            print("Sorry that item is not in the menu.")
+
+        more = input("Would you like to order anything else?: (yes/no)")
+        
+        if more.lower() != "yes":
+            break
+
+print(f"Your final order total is ₹{order_total}. Thank you for your order!")
